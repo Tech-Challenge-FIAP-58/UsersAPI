e3f653a0d5b6a5259251ca23d56ec6f9e0db7412
Thiago Olszewski <thiago.olszewskioc@gmail.com>
2026-03-22 17:00:11 -0400

    Test

--- src/FCG.Application/Producer/UserProducer.cs
+++ src/FCG.Application/Producer/UserProducer.cs
@@ -9,12 +9,14 @@ public class UserProducer(ILogger<UserProducer> logger, IBus bus)
     public async Task PublishUserCreatedEvent(int userId, string email)
     {
         var endpoint = await bus.GetSendEndpoint(new Uri("queue:notification-queue"));
+        logger.LogInformation("Publishing UserCreatedEvent for user ID: {UserId} to notification-queue.", userId);
         await endpoint.Send(new UserCreatedEvent
         {
             Destinatario = email,
             Assunto = "Bem-vindo ao FCG!",
             Corpo = $"Olá! Seu usuário com ID {userId} foi criado com sucesso."
         });
+        logger.LogInformation("UserCreatedEvent published for user ID: {UserId} to notification-queue.", userId);
 
         logger.LogInformation("UserCreatedEvent published to the notification-queue.");
 	}
