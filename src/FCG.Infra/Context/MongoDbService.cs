3d8a843e9429138c8b762f28730a73c66f03a2c2
thon18valentim <othonvvalentim@gmail.com>
2026-05-20 21:12:29 -0300

    InsertUserProducerEventLog

---
+++ src/FCG.Infra/Context/MongoDbService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace FCG.Infra.Context
+{
+	public class MongoDbService
+	{
+		private readonly IConfiguration _configuration;
+		private readonly IMongoDatabase? _database;
+
+		public MongoDbService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+
+			var connectionString = configuration.GetConnectionString("MongoDb");
+			var mongoUrl = MongoUrl.Create(connectionString);
+			var mongoClient = new MongoClient(mongoUrl);
+			_database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+		}
+
+		public IMongoDatabase? Database => _database;
+	}
+}
---
+++ src/FCG.Infra/Repository/IEventLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using FCG.Core.Models;
+
+namespace FCG.Infra.Repository
+{
+	public interface IEventLogRepository
+	{
+		Task InsertUserProducerEventLog(UserProducerEventLog log);
+	}
+}
